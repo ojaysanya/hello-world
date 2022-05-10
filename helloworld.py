30918ca183448a62d4be60e47a591ca43aed384d
ojaysanya <91535693+ojaysanya@users.noreply.github.com>
2022-05-10 13:49:32 +0100

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print("hello world")
+
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Middleware!')
+    }
